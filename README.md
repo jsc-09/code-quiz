a80749f74e1623ffa33c229af44852279173f480
Joanna Christian <joanna.s.christian@gmail.com>
2022-01-11 11:30:10 -0800

    created score of 0 when timer runs out of time.

--- README.md
+++ README.md
@@ -12,3 +12,16 @@ The scoreboard logs highscores using localstorage.
 URL: "https://jsc-09.github.io/code-quiz/"
 
 ![Screenshoot of site.](/images/codequiz-sample.png)
+
+Instructions on how the quiz works: 
+//FIRST: Quiz Instructions + Start Quiz Button 
+//SECOND: Once you hit Start Quiz Button, Question will appear with four answers.
+//SELECT ANSWER: Requires hover over effect. Each answer will be numbered 1. 2. 3. 4. 
+//CORRECT ASNWER will display "Correct!"" with <hr> line separating the question/answers from the alert and go onto the next question
+//WRONG ANSWER will display "Wrong!" with <hr> line separating the question/answers from the alert and go onto the next question
+//SET TIMER for 100 seconds
+//For each WRONG ANSWER, 10 seconds will be DEDUCTED from the timer.
+//When quiz is complete, display "ALL DONE" in heading. <p> Your high score: __ // and enter name for submit (input field).
+//SUBMIT name will bring a HIGHSCORE list. with options to START OVER or CLEAR SCORES
+//NAVIGATION BAR will include View Highscores option and Display Timer
+
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,3 @@
-//FIRST: Quiz Instructions + Start Quiz Button 
-//SECOND: Once you hit Start Quiz Button, Question will appear with four answers.
-//SELECT ANSWER: Requires hover over effect. Each answer will be numbered 1. 2. 3. 4. 
-//CORRECT ASNWER will display "Correct!"" with <hr> line separating the question/answers from the alert and go onto the next question
-//WRONG ANSWER will display "Wrong!" with <hr> line separating the question/answers from the alert and go onto the next question
-//SET TIMER for 100 seconds
-//For each WRONG ANSWER, 10 seconds will be DEDUCTED from the timer.
-//When quiz is complete, display "ALL DONE" in heading. <p> Your high score: __ // and enter name for submit (input field).
-//SUBMIT name will bring a HIGHSCORE list. with options to START OVER or CLEAR SCORES
-//NAVIGATION BAR will include View Highscores option and Display Timer
-
 let introContainer = document.querySelector(".intro-container");
 let startButton = document.querySelector(".start-btn");
 let questionContainer = document.querySelector('.question-container');
@@ -42,12 +31,21 @@ viewHighScoreLink.addEventListener("click", showHighScoreList );
 
 //Countdown Timer 
 function countdown() {
-    timeLeft = 100;
+    timeLeft = 60;
     //set timer countdown
     timeInterval = setInterval(function (){
+    
+    if(timeLeft > 0) {
         timeLeft--;
+        timerDisplay.textContent = timeLeft; 
+    }
+    else if (timeLeft ==0) {
         timerDisplay.textContent = timeLeft;
-    }, 1000);
+        reset();
+        clearInterval(timeInterval);
+        showFinalScore();
+    }
+ }, 1000);
 }
 
 console.log(countdown)
@@ -107,8 +105,7 @@ function selectAnswer(event) {
     }
 //if wrong, display "Wrong!"
     else {
-        correctWrong.textContent = "Wrong!";
-        
+        correctWrong.textContent = "Wrong!";        
         correctWrong.classList.add('answer')
         timeLeft = timeLeft-10;
     }
